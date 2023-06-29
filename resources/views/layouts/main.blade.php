43f0045cafdc82259f2aa54bbf18cd6956771d91
StarLordCraft <rafa.viana.moc@gmail.com>
2023-06-29 05:37:11 -0300

    Importing BootStrap-CSS- and FONT (0% creative font...)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('title')</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href=@yield('style')>
     </head>
     <body >
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <h1>OLA MUNDO</h1>
+
 @endsection
 
 @section('script', "js/welcome/main.js")
