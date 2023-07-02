ab060540e1256c3643fd5adcad14c6d422bd402a
StarLordCraft <rafa.viana.moc@gmail.com>
2023-07-01 22:47:51 -0300

    I decided to build by myself the authentication so this is the logic of the table user!

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->json('events');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
