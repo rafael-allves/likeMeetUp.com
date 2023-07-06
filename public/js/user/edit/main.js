636e1876a9c96365c1b6d91bea3cfdbe57b5908c
StarLordCraft <rafa.viana.moc@gmail.com>
2023-07-06 19:56:09 -0300

    OKAY 80% functional in desktop, but 50% in mobile, need to fix it

---
+++ public/js/user/edit/main.js
@@ -0,0 +1,24 @@
+const profilePic = document.getElementById('user-pic-tab');
+const btnUploadFile = document.getElementById('btnUpFile');
+const file = document.getElementById('file');
+
+
+profilePic.addEventListener('mouseenter', () =>{
+    btnUploadFile.style.display = "flex";
+    profilePic.style.filter = 'blur(3px)';
+})
+
+btnUploadFile.addEventListener('mouseleave', () =>{
+    btnUploadFile.style.display = "none";
+    profilePic.style.filter = 'blur(0)'
+})
+
+profilePic.addEventListener('touchstart', ()=>{
+    btnUploadFile.style.display = "flex";
+    profilePic.style.filter = 'blur(3px)';
+})
+
+file.addEventListener('change', () =>{
+    btnUploadFile.style.display = "none";
+    profilePic.style.filter = 'blur(0)'
+})
