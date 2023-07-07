34d1a9ebc23ae4399d0941b484eaabfd2ab22507
StarLordCraft <rafa.viana.moc@gmail.com>
2023-07-07 14:59:01 -0300

    Bug fixed: BIO WIHOUT DEFAULT VALUE

--- database/migrations/2023_07_06_190657_add_bio_to_users_table.php
+++ database/migrations/2023_07_06_190657_add_bio_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('bio');
+            $table->text('bio')->default('');
         });
     }
 
