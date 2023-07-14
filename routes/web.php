1b3d08213a8be55aea2cf06819c9e5709e148ccd
Rafael <rafa.viana.moc@gmail.com>
2023-07-14 17:06:59 -0300

    Refactoring: backend refactored properly!

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\UserController;
 
-
-
 Route::resource('events', EventController::class)->name('', 'events');
-Route::post('/events/join/{evento}', [EventController::class, 'joinEvent'])->middleware('auth');
-Route::delete('/events/leave/{evento}', [EventController::class, 'leaveEvent'])->middleware('auth');
+Route::controller(EventController::class)->middleware(['auth'])->group(function (){
+    Route::post('/events/join/{evento}', 'joinEvent');
+    Route::delete('/events/leave/{evento}', 'leaveEvent');
+});
 
 Route::resource('users', UserController::class)->name('', 'users');
-Route::post('/auth/register', [UserController::class, 'register']);
-Route::post('/auth/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
-Route::get('/dashboard', [UserController::class, 'dashboard'])->middleware('auth');
+Route::controller(UserController::class)->group(function (){
+    Route::post('/auth/register', 'register');
+    Route::post('/auth/login', 'login');
+
+});
+
+Route::controller(UserController::class)->middleware(['auth'])->group(function (){
+    Route::post('/logout', 'logout');
+    Route::get('/dashboard', 'dashboard');
+});
