6c87289627f5f2f77e794eb335e5baf4f83c1c97
Rafael <rafa.viana.moc@gmail.com>
2023-07-17 00:59:40 -0300

    Styling the navbar

--- resources/js/Components/navbar.vue
+++ resources/js/Components/navbar.vue
@@ -5,15 +5,33 @@
 </script>
 
 <template>
-    <header class="flex flex-row shadow-md px-10">
-        <nav>
-            <Link href="/">
-                <img width="40" height="40" :src=Logo alt="Logo">
-            </Link>
+    <header class="flex flex-row spaceItem shadow-md px-10">
+        <Link href="/">
+            <img width="40" height="40" :src=Logo alt="Logo">
+        </Link>
+        <nav class="flex flex-row">
+            <div class="flex flex-row gap-3">
+                <Link href="/about" class="hover:text-colorPrimary">
+                    Sobre
+                </Link>
+                <Link href="/Contact" class="hover:text-colorPrimary">
+                    Contato
+                </Link>
+                <Link href="/Contact" class="hover:text-colorPrimary">
+                    Eventos
+                </Link>
+            </div>
+            <button>
+
+            </button>
+            
         </nav>
     </header>
 </template>
 
 <style scoped>
-
+    .spaceItem{
+        align-items: center;
+        justify-content: space-between !important;
+    }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                colorPrimary: "#F2A340",
+            }
         },
     },
 
