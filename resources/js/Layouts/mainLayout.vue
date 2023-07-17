d9bd5c0e8ddfe15219f02ae104f1df873d9880da
Rafael <rafa.viana.moc@gmail.com>
2023-07-17 02:49:50 -0300

    Page auth made

--- resources/js/Layouts/mainLayout.vue
+++ resources/js/Layouts/mainLayout.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import {Head} from '@inertiajs/vue3';
+
     import Navbar from '../Components/navbar.vue';
     const props = defineProps({
         authStatus:{
@@ -14,6 +16,9 @@
 
 <template>
     <div>
+        <Head>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        </Head>
         <Navbar 
             :authStatus="props.authStatus"
             :user="props.user"
--- resources/js/Pages/Auth.vue
+++ resources/js/Pages/Auth.vue
@@ -1,5 +1,10 @@
 <script setup>
-    import Layout from '../Layouts/mainLayout.vue'
+    import Layout from '../Layouts/mainLayout.vue';
+    
+    import '../../../public/css/auth/styles.css';
+
+    import Logo from '../../../public/assets/logo.png'
+
     const props = defineProps({
         user:{
             type: Object,
@@ -14,173 +19,140 @@
     :user="props.user"
     
     >
-    <main>
-        <section class="main__container">
-                <header class="main__container__header">
-                    <h2 class="main__container__header_h2">
-                        <img src="/assets/logo.png" width="40px" height="40px" alt="Rafa Events" />
-                        Rafa Events
-                    </h2>
+        <main>
+            <section class="main__container">
+                    <header class="main__container__header">
+                        <h2 class="main__container__header_h2">
+                            <img :src=Logo width="40px" height="40px" alt="Rafa Events" />
+                            Rafa Events
+                        </h2>
 
-                    <h1 class="main__container__header_h1">
-                        Bem-vindo
-                    </h1>
+                        <h1 class="main__container__header_h1">
+                            Bem-vindo
+                        </h1>
 
-                    <small>Por Favor, Informe suas credenciais</small>
-                </header>
-                <section>
-                    <section class="main__container__form__section">
-                        <div class="main__container__form__section_authtype">
-                            <button
-                            type="button"
-                            id="login">
-                                Login
-                            </button>
-                        </div>
-                        <span class="authSelected">Login</span>
-                        <div class="main__container__form__section_authtype">
-                            <button
-                            type="button"
-                            id="register"
-                            >
-                                Register
-                            </button>
-                        </div>
-                    </section>
+                        <small>Por Favor, Informe suas credenciais</small>
+                    </header>
                     <section>
-                        <form id="loginform" class="auth" method="POST">
-                            @csrf
-                            <div class="input__camp">
-                                <span class="material-symbols-outlined input__camp_icon">
-                                    mail
-                                </span>
-                                <div class="input__text">
-                                    <label for="loginemail">Email</label>
-                                    <input type="email" name="loginemail" id="loginemail" class="input__camp_input" required maxlength="64">
-                                    <span class="material-symbols-outlined">
-
+                        <section class="main__container__form__section">
+                            <div class="main__container__form__section_authtype">
+                                <button
+                                type="button"
+                                id="login">
+                                    Login
+                                </button>
+                            </div>
+                            <span class="authSelected">Login</span>
+                            <div class="main__container__form__section_authtype">
+                                <button
+                                type="button"
+                                id="register"
+                                >
+                                    Register
+                                </button>
+                            </div>
+                        </section>
+                        <section>
+                            <form id="loginform" class="auth" method="POST">
+                                <div class="input__camp">
+                                    <span class="material-symbols-outlined input__camp_icon">
+                                        mail
                                     </span>
-                                </div>
-                            </div>
-                            <p class="errorMessage">
-                            </p>
-                            <div class="input__camp">
-                                <span class="material-symbols-outlined input__camp_icon">
-                                    lock
-                                </span>
-                                <div class="input__text">
-                                    <label for="loginpassword">Senha</label>
-                                    <input type="password" name="loginpassword" id="loginpassword" class="input__camp_input" required minlength="8">
-                                    <button class="showpassbtn" type="button">
+                                    <div class="input__text">
+                                        <label for="loginemail">Email</label>
+                                        <input type="email" name="loginemail" id="loginemail" class="input__camp_input" required maxlength="64">
                                         <span class="material-symbols-outlined">
-                                            visibility_off
+
                                         </span>
-                                    </button>
+                                    </div>
                                 </div>
-                            </div>
-                            <p class="errorMessage"></p>
-                            <button id="formLogin" class="authButton" type="submit">LOGIN</button>
-
-                            <footer class="auth__options">
-                                <fieldset class="auth__options_fieldset">
-                                    <legend>Or Continue With</legend>
-                                            <button>
-
-                                            </button>
-                                            <button>
-
-                                            </button>
-                                            <button>
-
-                                            </button>
-                                </fieldset>
-                            </footer>
-
-                        </form>
-                        <form class="auth" id="registerform" method="POST">
-                            @csrf
-                            <div class="input__camp">
-                                <span class="material-symbols-outlined input__camp_icon">
-                                    person
-                                </span>
-                                <div class="input__text">
-                                    <label for="registerusername">Nome</label>
-                                    <input type="username" name="registerusername" id="registerusername" class="input__camp_input" required minlength="4">
-                                    <span class="material-symbols-outlined">
-
+                                <p class="errorMessage">
+                                </p>
+                                <div class="input__camp">
+                                    <span class="material-symbols-outlined input__camp_icon">
+                                        lock
                                     </span>
+                                    <div class="input__text">
+                                        <label for="loginpassword">Senha</label>
+                                        <input type="password" name="loginpassword" id="loginpassword" class="input__camp_input" required minlength="8">
+                                        <button class="showpassbtn" type="button">
+                                            <span class="material-symbols-outlined">
+                                                visibility_off
+                                            </span>
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                            <p class="errorMessage"></p>
-                            <div class="input__camp">
-                                <span class="material-symbols-outlined input__camp_icon">
-                                    mail
-                                </span>
-                                <div class="input__text">
-                                    <label for="registeremail">Email</label>
-                                    <input type="email" name="registeremail" id="registeremail" class="input__camp_input" required maxlength="64">
-                                    <span class="material-symbols-outlined">
+                                <p class="errorMessage"></p>
+                                <button id="formLogin" class="authButton" type="submit">LOGIN</button>
 
+                            </form>
+                            <form class="auth" id="registerform" method="POST">
+                                <div class="input__camp">
+                                    <span class="material-symbols-outlined input__camp_icon">
+                                        person
                                     </span>
-                                </div>
-                            </div>
-                            <p class="errorMessage"></p>
-                            <div class="input__camp">
-                                <span class="material-symbols-outlined input__camp_icon">
-                                    lock
-                                </span>
-                                <div class="input__text">
-                                    <label for="registerpassword">Senha</label>
-                                    <input type="password" name="registerpassword" id="registerpassword" class="input__camp_input" required minlength="8">
-                                    <button class="showpassbtn" type="button">
+                                    <div class="input__text">
+                                        <label for="registerusername">Nome</label>
+                                        <input type="username" name="registerusername" id="registerusername" class="input__camp_input" required minlength="4">
                                         <span class="material-symbols-outlined">
-                                            visibility_off
-                                        </span>
-                                    </button>
-                                </div>
-                            </div>
-                            <p class="errorMessage"></p>
 
-                            <div class="input__camp">
-                                <span class="material-symbols-outlined input__camp_icon">
-                                    lock
-                                </span>
-                                <div class="input__text">
-                                    <label for="confirmpassword">Confirmar Senha</label>
-                                    <input type="password" name="confirmpassword" id="confirmpassword" class="input__camp_input" required>
-                                    <button class="showpassbtn" type="button">
+                                        </span>
+                                    </div>
+                                </div>
+                                <p class="errorMessage"></p>
+                                <div class="input__camp">
+                                    <span class="material-symbols-outlined input__camp_icon">
+                                        mail
+                                    </span>
+                                    <div class="input__text">
+                                        <label for="registeremail">Email</label>
+                                        <input type="email" name="registeremail" id="registeremail" class="input__camp_input" required maxlength="64">
                                         <span class="material-symbols-outlined">
-                                            visibility_off
+
                                         </span>
-                                    </button>
+                                    </div>
                                 </div>
-                            </div>
-                            <p class="errorMessage"></p>
+                                <p class="errorMessage"></p>
+                                <div class="input__camp">
+                                    <span class="material-symbols-outlined input__camp_icon">
+                                        lock
+                                    </span>
+                                    <div class="input__text">
+                                        <label for="registerpassword">Senha</label>
+                                        <input type="password" name="registerpassword" id="registerpassword" class="input__camp_input" required minlength="8">
+                                        <button class="showpassbtn" type="button">
+                                            <span class="material-symbols-outlined">
+                                                visibility_off
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <p class="errorMessage"></p>
 
-                            <button id="formRegister" class="authButton" type="submit">REGISTER</button>
+                                <div class="input__camp">
+                                    <span class="material-symbols-outlined input__camp_icon">
+                                        lock
+                                    </span>
+                                    <div class="input__text">
+                                        <label for="confirmpassword">Confirmar Senha</label>
+                                        <input type="password" name="confirmpassword" id="confirmpassword" class="input__camp_input" required>
+                                        <button class="showpassbtn" type="button">
+                                            <span class="material-symbols-outlined">
+                                                visibility_off
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <p class="errorMessage"></p>
 
-                            <footer class="auth__options">
-                                <fieldset class="auth__options_fieldset">
-                                    <legend>Or Continue With</legend>
-                                            <button>
-
-                                            </button>
-                                            <button>
-
-                                            </button>
-                                            <button>
-
-                                            </button>
-                                </fieldset>
-                            </footer>
-                        </form>
+                                <button id="formRegister" class="authButton" type="submit">REGISTER</button>
+                            </form>
+                        </section>
                     </section>
-                </section>
-        </section>
-    </main>
+            </section>
+        </main>
     </Layout>
 </template>
 
-<style scoped>
-
+<style>
 </style>
