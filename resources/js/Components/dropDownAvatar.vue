139a0cc6573003c91ede62212c613d32c276661c
Rafael <rafa.viana.moc@gmail.com>
2023-07-18 12:30:46 -0300

    Component dropDownAvatar to improve readability

---
+++ resources/js/Components/dropDownAvatar.vue
@@ -0,0 +1,52 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    
+    import ProfilePic from './profilepic.vue';
+    
+    const props = defineProps({
+        user:{
+            type: Object,
+        }
+    });
+
+    const linkEditUser = `users/${props.user.id}/edit`;
+</script>
+
+<template>
+        <section id="dropdownAvatar" class="z-1 right-3 hidden bg-white px-7 divide-y divide-gray-100 rounded-lg shadow w-44 absolute align-middle border">
+            <h2 class="text-sm text-gray-900 flex gap-1 items-center border-b-2 pb-1 mt-3">
+                <ProfilePic :profilePic="props.user.profile_pic" />
+                {{ props.user.name }}
+            </h2>
+            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 flex flex-col gap-1">
+                <li>
+                    <Link :href="linkEditUser" class="text-black flex items-center gap-2 hover:border-l-2 border-colorSecondary hover:pl-1">
+                        <span class="material-symbols-outlined">
+                            person
+                        </span>
+                        Editar perfil
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/dashboard" class="text-black flex items-center gap-2 hover:border-l-2 border-colorSecondary hover:pl-1">
+                        <span class="material-symbols-outlined">
+                            dashboard
+                        </span>
+                        Dashboard
+                    </Link>
+                </li>
+                <li class="my-3 border-t-2 py-2">
+                    <Link href="/logout" class="text-black flex items-center gap-2 hover:border-l-2 border-colorSecondary hover:pl-1">
+                        <span class="material-symbols-outlined">
+                            logout
+                        </span>
+                        Sair
+                    </Link>
+                </li>
+            </ul>
+        </section>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Components/navbar.vue
+++ resources/js/Components/navbar.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { Link } from '@inertiajs/vue3';
-    import ProfilePic from './profilepic.vue';
+    import DropDownAvatar from './dropDownAvatar.vue'
+    import ProfilePic from './profilepic.vue'
 
     import Logo from '/public/assets/logo.png';
     
@@ -14,8 +15,6 @@
             type: Object,
         }
     });
-
-    const linkEditUser = `users/${props.user.id}/edit`;
 </script>
 
 <template>
@@ -29,7 +28,7 @@
             </button>
 
             <div class="hidden menu max-md:flex max-md:justify-center max-md:items-center max-md:flex-col w-full absolute top-8 right-0 z-10 transition delay-300">
-
+                
             </div>
 
             <div class="gap-3 max-md:hidden flex flex-row">
@@ -49,38 +48,7 @@
                         <span class="sr-only">Open user menu</span>
                         <ProfilePic :profilePic="props.user.profile_pic" />
                     </button>
-                    <div id="dropdownAvatar" class="z-1 right-3 hidden bg-white px-7 divide-y divide-gray-100 rounded-lg shadow w-44 absolute align-middle border">
-                        <h2 class="text-sm text-gray-900 flex gap-1 items-center border-b-2 pb-1 mt-3">
-                            <ProfilePic :profilePic="props.user.profile_pic" />
-                            {{ props.user.name }}
-                        </h2>
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 flex flex-col gap-1">
-                            <li>
-                                <Link :href="linkEditUser" class="text-black flex items-center gap-2 hover:border-l-2 border-colorSecondary hover:pl-1">
-                                    <span class="material-symbols-outlined">
-                                        person
-                                    </span>
-                                    Editar perfil
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/dashboard" class="text-black flex items-center gap-2 hover:border-l-2 border-colorSecondary hover:pl-1">
-                                    <span class="material-symbols-outlined">
-                                        dashboard
-                                    </span>
-                                    Dashboard
-                                </Link>
-                            </li>
-                            <li class="my-3 border-t-2 py-2">
-                                <Link href="/logout" class="text-black flex items-center gap-2 hover:border-l-2 border-colorSecondary hover:pl-1">
-                                    <span class="material-symbols-outlined">
-                                        logout
-                                    </span>
-                                    Sair
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                    <DropDownAvatar :user="props.user" />
                 </div>
                 <Link v-else href="/users/create">
                     Login/Registro
