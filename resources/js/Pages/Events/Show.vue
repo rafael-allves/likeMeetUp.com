efdca71752ad95d54533c2b38fba0bff713cf0d5
Rafael <rafa.viana.moc@gmail.com>
2023-07-27 03:06:20 -0300

    Events-show: Page DOne

--- resources/js/Pages/Events/Show.vue
+++ resources/js/Pages/Events/Show.vue
@@ -47,110 +47,139 @@
             {{ props.event.title }}
         </title>
     </Head>
-        <main class="md:flex w-full mt-4">
-            <div class="md:w-[50%] w-full min-w-[300px] flex justify-center">
-                <img :src="'/' + props.event.image" alt="Event Image"
-                class="object-fit flex justify-center">
-            </div>
-            <div>
-                <h1 class="text-3xl font-bold">
-                    {{ props.event.title }}
-                </h1>
-                <h3 class="text-xl flex text-center items-center gap-3 mt-2">
-                    <span class="material-symbols-outlined text-textMuted">
-                        calendar_month
-                    </span>
-                    {{ 
-                        `
-                            ${ new Date(props.event.date).getDay().toString().length != 1 ? 
-                                new Date(props.event.date).getDay() :
-                                '0'.concat(new Date(props.event.date).getDay().toString()) }
-                            /
-                            ${(new Date(props.event.date).getMonth() + 1).toString().length != 1 ?
-                                new Date(props.event.date).getMonth() + 1 :
-                                '0'.concat(new Date(props.event.date).getMonth() + 1)}
-                            /
-                            ${new Date(props.event.date).getFullYear()}
-                        ` 
-                    }}
-                </h3>
-
-                <h3 class="text-xl flex text-center items-center gap-3 mt-2">
-                    <span class="material-symbols-outlined text-textMuted">
-                        location_on
-                    </span>
-                    {{ props.event.local }}
-                </h3>
-
-                <h2 class="text-xl font-bold">
-                    <Link :href="`/users/${props.dono.id}`"
-                    class="mt-3 flex text-center items-center gap-2">
-                        <span class="material-symbols-outlined text-yellow-600">
-                            star
+        <main class="md:px-20">
+            <section class="md:flex w-full mt-4">
+                <section class="md:w-[50%] w-full min-w-[300px] flex justify-center">
+                    <img :src="'/' + props.event.image" alt="Event Image"
+                    class="object-fit flex justify-center">
+                </section>
+                <section>
+                    <h1 class="text-3xl font-bold">
+                        {{ props.event.title }}
+                    </h1>
+                    <h3 class="text-xl flex text-center items-center gap-3 mt-2">
+                        <span class="material-symbols-outlined text-textMuted">
+                            calendar_month
                         </span>
-                        <img :src="props.dono.profile_pic" 
-                        alt="Perfil do Dono do Evento"
-                        width="40"
-                        height="40"
-                        />
-                        {{ props.dono.name }}
-                    </Link>
-                    <section v-if="props.user.id === props.dono.id"
-                    class="flex mt-3 gap-2 px-2">
-                        <Link class="bg-colorSecondary w-[50%] py-2 shadow-black shadow-xl text-white flex items-center gap-4 justify-center"
-                        :href="`/events/${props.event.id}/edit`">
-                            Editar
-                            <span class="material-symbols-outlined">
-                                edit
+                        {{ 
+                            `
+                                ${ new Date(props.event.date).getDay().toString().length != 1 ? 
+                                    new Date(props.event.date).getDay() :
+                                    '0'.concat(new Date(props.event.date).getDay().toString()) }
+                                /
+                                ${(new Date(props.event.date).getMonth() + 1).toString().length != 1 ?
+                                    new Date(props.event.date).getMonth() + 1 :
+                                    '0'.concat(new Date(props.event.date).getMonth() + 1)}
+                                /
+                                ${new Date(props.event.date).getFullYear()}
+                            ` 
+                        }}
+                    </h3>
+    
+                    <h3 class="text-xl flex text-center items-center gap-3 mt-2">
+                        <span class="material-symbols-outlined text-textMuted">
+                            location_on
+                        </span>
+                        {{ props.event.local }}
+                    </h3>
+    
+                    <h2 class="text-xl font-bold">
+                        <Link :href="`/users/${props.dono.id}`"
+                        class="mt-3 flex text-center items-center gap-2">
+                            <span class="material-symbols-outlined text-yellow-600">
+                                star
                             </span>
+                            <img :src="props.dono.profile_pic" 
+                            alt="Perfil do Dono do Evento"
+                            width="40"
+                            height="40"
+                            />
+                            {{ props.dono.name }}
                         </Link>
-                        <form
-                        @submit.prevent=""
-                        class="w-[50%]"
-                        >
-                            <button 
-                            type="submit"
-                            class="bg-red-700 w-full py-2 shadow-black shadow-xl text-white flex items-center gap-4 justify-center"
+                        <section v-if="props.user.id === props.dono.id"
+                        class="flex mt-3 gap-2 px-2">
+                            <Link class="bg-colorSecondary w-[50%] py-2 shadow-black shadow-xl text-white flex items-center gap-4 justify-center"
+                            :href="`/events/${props.event.id}/edit`">
+                                Editar
+                                <span class="material-symbols-outlined">
+                                    edit
+                                </span>
+                            </Link>
+                            <form
+                            @submit.prevent=""
+                            class="w-[50%]"
                             >
-                            Excluir
-                            <span class="material-symbols-outlined">
-                                delete
-                            </span>
+                                <button 
+                                type="submit"
+                                class="bg-red-700 w-full py-2 shadow-black shadow-xl text-white flex items-center gap-4 justify-center"
+                                >
+                                Excluir
+                                <span class="material-symbols-outlined">
+                                    delete
+                                </span>
+                                </button>
+                            </form>
+                        </section>
+                    </h2>
+                    <p class="flex items-center gap-2">
+                        <span class="material-symbols-outlined text-textMuted">
+                            group
+                        </span>
+                        {{ props.event.users.length }}
+                        {{props.event.users.length == 1 ? 'Participante' : 'Participantes'}}
+                    </p>
+                    <div class="md:mt-3 mt-6">
+                        <form 
+                        v-if="props.event.users.filter((user) => user.id == props.user.id).length == 0"
+                        @submit.prevent="join">
+                            <button type="submit"
+                            class="bg-colorPrimary flex items-center gap-3 text-white font-bold px-4 py-2 rounded shadow-lg shadow-black">
+                                Entrar no Evento
+                                <span class="material-symbols-outlined">
+                                    door_open
+                                </span>
                             </button>
                         </form>
-                    </section>
-                </h2>
-                <p class="flex items-center gap-2">
-                    <span class="material-symbols-outlined text-textMuted">
-                        group
-                    </span>
-                    {{ props.event.users.length }}
-                    {{props.event.users.length == 1 ? 'Participantes' : 'Participante'}}
-                </p>
-                <div class="md:mt-3 mt-6">
-                    <form 
-                    v-if="props.event.users.filter((user) => user.id == props.user.id).length == 0"
-                    @submit.prevent="join">
-                        <button type="submit"
-                        class="bg-colorPrimary flex items-center gap-3 text-white font-bold px-4 py-2 rounded shadow-lg shadow-black">
-                            Entrar no Evento
-                            <span class="material-symbols-outlined">
-                                door_open
-                            </span>
-                        </button>
-                    </form>
-                    <form v-else
-                    @submit.prevent="leave">
-                        <button type="submit"
-                        class="bg-red-700 text-white px-3 py-2 font-bold flex items-center gap-2 rounded shadow-lg shadow-black">
-                            Sair do Evento
-                            <span class="material-symbols-outlined">
-                                delete
-                            </span>
-                        </button>
-                    </form>
-                </div>
-            </div>
+                        <form v-else
+                        @submit.prevent="leave">
+                            <button type="submit"
+                            class="bg-red-700 text-white px-3 py-2 font-bold flex items-center gap-2 rounded shadow-lg shadow-black">
+                                Sair do Evento
+                                <span class="material-symbols-outlined">
+                                    delete
+                                </span>
+                            </button>
+                        </form>
+                    </div>
+                </section>
+
+            </section>
+            <section class="mt-5">
+                <section 
+                v-if="props.event.items.length > 0"
+                >
+                    <h2 
+                    class="text-2xl font-extrabold" 
+                    >
+                        O Evento Conta Com:
+                    </h2>
+                    <div class="flex flex-wrap">
+                        <h3 v-for="item in props.event.items"
+                        class="flex items-center gap-1">
+                        <span class="material-symbols-outlined text-yellow-600">
+                            play_arrow
+                        </span>
+                        {{ item }}
+                        </h3>
+                    </div>
+                </section>
+                <section class="mt-5">
+                    <h2 class="text-2xl font-extrabold">
+                        Sobre O Evento:
+                    </h2>
+                    {{ props.event.description }}
+                </section>
+            </section>
         </main>
    </Layout>
 </template>
